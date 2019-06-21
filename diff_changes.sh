5f197877a30b4a3011a34dc8b7293a3d89c1afbf
Rodger Oliver <timeline.mb@gmail.com>
2019-06-21 17:32:26 -0300

    diff script remove unnecessary feedback

--- diff_changes.sh
+++ diff_changes.sh
@@ -7,15 +7,9 @@ for remote_file in $(find .); do
 		if [ -f $local_file ]; then
 			local_hash=$(md5sum $local_file | sed "s/\ .*//g");
 			remote_hash=$(md5sum $remote_file | sed "s/\ .*//g");
-			# echo $remote_file;
-			# echo $local_file;
-			echo $remote_hash;
-			echo $local_hash;
 			if [[ $local_hash != $remote_hash ]]; then
 				vimdiff $local_file $remote_file;
-				# echo "different";
 			fi
-			echo "==============================";
 		fi
 	fi
 done

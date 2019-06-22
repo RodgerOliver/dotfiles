84038f6ddac5abde6f5f7acb5afa92d6c3b08ae7
Rodger Oliver <timeline.mb@gmail.com>
2019-06-21 21:43:38 -0300

    diff script changed find cmd and added feedback

--- diff_changes.sh
+++ diff_changes.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# find . -type f ! -path './.git/*'
-
-for remote_file in $(find .); do
+for remote_file in $(find . -type f ! -path './.git/*' ! -path './.icons/*'); do
 	home_path=$(echo $HOME | sed 's/\//\\\//g')
-	if [ -f $remote_file ]; then
 		local_file=$(echo $remote_file | sed "s/^\./$home_path/");
 		if [ -f $local_file ]; then
 			local_hash=$(md5sum $local_file | sed "s/\ .*//g");
 			remote_hash=$(md5sum $remote_file | sed "s/\ .*//g");
 			if [[ $local_hash != $remote_hash ]]; then
 				vimdiff $local_file $remote_file;
+				echo "UPDATED $(echo $local_file | sed "s/$home_path/~/")";
 			fi
 		fi
-	fi
 done

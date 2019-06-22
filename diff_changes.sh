f1b04d7e86dc3015223d720697a7ca0d207f6e00
Rodger Oliver <timeline.mb@gmail.com>
2019-06-22 08:18:09 -0300

    diff script add feedback no file

--- diff_changes.sh
+++ diff_changes.sh
@@ -3,12 +3,15 @@
 for remote_file in $(find . -type f ! -path './.git/*' ! -path './.icons/*'); do
 	home_path=$(echo $HOME | sed 's/\//\\\//g')
 		local_file=$(echo $remote_file | sed "s/^\./$home_path/");
+		home_file=$(echo $remote_file | sed "s/^\./~/");
 		if [ -f $local_file ]; then
 			local_hash=$(md5sum $local_file | sed "s/\ .*//g");
 			remote_hash=$(md5sum $remote_file | sed "s/\ .*//g");
 			if [[ $local_hash != $remote_hash ]]; then
 				vimdiff $local_file $remote_file;
-				echo "UPDATED $(echo $local_file | sed "s/$home_path/~/")";
+				echo "UPDATED ${home_file}";
 			fi
+		else
+				echo "NO FILE ${home_file}";
 		fi
 done
